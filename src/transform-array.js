6a07ec2c9de1b6323fba7a05bb74451bcf6538fb
freenokke <kosmatkov94@gmail.com>
2022-04-28 21:09:17 +0300

    feat: transform-array  are resolved

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  if(!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!")
+  let copyArr = [...arr]
+  copyArr.map((item, index, array) => {
+    console.log(`Array ${array}, Item ${item}, Index ${index}`)
+      switch (item) {
+        case '--discard-next':
+          if (typeof array[index+1] == 'number') {
+            array.splice(index, 2)
+          }
+          if (typeof array[index] == 'string') array.splice(index,1)
+          break;
+        case '--discard-prev':
+          if (array[index-1]) array.splice(index - 1, 2)
+          else array.splice(index, 1)
+          break;
+        case '--double-next':
+          if (array[index+1]) array.splice(index, 1, array[index + 1])
+          else array.splice(index, 1)
+          break;
+        case '--double-prev':
+          if (array[index-1]) array.splice(index, 1, array[index - 1])
+          else array.splice(index, 1)
+          break;
+        default:
+          break
+      }
+  })
+  return copyArr
 }
 
 module.exports = {
