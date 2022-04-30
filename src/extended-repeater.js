e39491b3889d965094f16a2e566fb9d077c23cad
freenokke <kosmatkov94@gmail.com>
2022-04-30 14:48:57 +0300

    feat: extended-repeater are solved

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (!typeof options.repeatTimes == 'number' || !Number.isInteger(options.repeatTimes)) options.repeatTimes = 1
+  if (!typeof options.additionRepeatTimes == 'number' || !Number.isInteger(options.additionRepeatTimes)) options.additionRepeatTimes = 1
+  if ('addition' in options && options.addition == null) options.addition = 'null'
+
+
+  let additionsArr = []
+  for (let i=0; i < options.additionRepeatTimes; i++) {
+    additionsArr.push(options.addition)
+  }
+  joinedAdditionsArr = additionsArr.join(options.additionSeparator || '|')
+  let stringArr = []
+  for (let i=0; i < options.repeatTimes; i++) {
+    stringArr.push(str + joinedAdditionsArr)
+}
+  joinedStringArr = stringArr.join(options.separator || '+')
+  return joinedStringArr
 }
 
 module.exports = {
