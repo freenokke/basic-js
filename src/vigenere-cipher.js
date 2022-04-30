d5cf3b94d85cfca10ff916afe2921f03f70711f4
freenokke <kosmatkov94@gmail.com>
2022-04-30 21:00:47 +0300

    feat: vigenere-cipher are solved

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,86 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor (isDirect = true) {
+    this.reverse = !isDirect
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    if(str === undefined || key === undefined) throw new Error('Incorrect arguments!')
+
+    if(this.reverse == true) {
+      str = str.split('').reverse().join('')
+      key = key.split('').reverse().join('')
+    }
+
+    const a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";	
+
+    let trimmedStr = str.replace(/\s/g, '')
+    key = key.repeat(Math.floor(trimmedStr.length / key.length) ? trimmedStr.length / key.length : 1);
+    key = (key + key.substr(0,trimmedStr.length % key.length)).toUpperCase()
+    str = str.toUpperCase()
+    let keyArr = key.split('')
+    let strArr = str.split('')
+    strArr.forEach((element, index) => {
+      if (element == ' ') {
+        keyArr.splice(index,0, element)
+      }
+    });
+
+
+    let finalStr = [];
+
+    for (let i = 0; i < strArr.length; i++) {
+      if (!a.includes(str[i])) {
+        finalStr.push(str[i])
+      } else {
+      let position = a.indexOf(strArr[i])
+      let shift = a.indexOf(keyArr[i])
+      finalStr.push(a[(position + shift) % 26])
+      }
+    }
+
+    return finalStr.join('')
+  }
+
+  decrypt(str, key) {
+    if(str === undefined || key === undefined) throw new Error('Incorrect arguments!')
+
+    if(this.reverse == true) {
+      str = str.split('').reverse().join('')
+      key = key.split('').reverse().join('')
+    }
+
+    const a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";	
+
+    let trimmedStr = str.replace(/\s/g, '')
+    key = key.repeat(Math.floor(trimmedStr.length / key.length) ? trimmedStr.length / key.length : 1);
+    key = (key + key.substr(0,trimmedStr.length % key.length)).toUpperCase()
+    str = str.toUpperCase()
+    let keyArr = key.split('')
+    let strArr = str.split('')
+    strArr.forEach((element, index) => {
+      if (element == ' ') {
+        keyArr.splice(index,0, element)
+      }
+    });
+
+    let finalStr = [];
+
+    for (let i = 0; i < strArr.length; i++) {
+      if (!a.includes(str[i])) {
+        finalStr.push(str[i])
+      } else {
+      let position = a.indexOf(strArr[i])
+      let shift = a.indexOf(keyArr[i])
+      finalStr.push(a.slice((position - shift), (position - shift) == -1 ? undefined : (position - shift) + 1 )[0])
+      }
+    }
+
+    return finalStr.join('')
+
+
   }
 }
 
